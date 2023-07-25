1973f09f98468d509b11e6827366af88fba8e06c
Dev Theofanes <devtheofanes@gmail.com>
2023-07-25 07:47:52 -0300

    refactor: change nomenclature of user repo in tests for sut

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -4,21 +4,21 @@ import { InMemoryUserRepository } from './in-memory-user-repository'
 describe('In memory User repository', () => {
   test('should null if user is not found', async () => {
     const users: UserData[] = []
-    const userRepo = new InMemoryUserRepository(users)
+    const sut = new InMemoryUserRepository(users)
 
-    const user = await userRepo.findUserByEmail('')
+    const user = await sut.findUserByEmail('')
     expect(user).toBeNull()
   })
 
   test('should user if it is found in the repository', async () => {
     const users: UserData[] = []
-    const userRepo = new InMemoryUserRepository(users)
+    const sut = new InMemoryUserRepository(users)
 
     const name = 'any name'
     const email = 'any@mail.com'
 
-    await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail(email)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail(email)
 
     expect(user?.name).toBe(name)
   })
@@ -28,9 +28,9 @@ describe('In memory User repository', () => {
       { name: 'any_name', email: 'any@mail.com' },
       { name: 'second_name', email: 'second@mail.com' }
     ]
-    const userRepo = new InMemoryUserRepository(users)
+    const sut = new InMemoryUserRepository(users)
 
-    const allUsers = await userRepo.findAllUsers()
+    const allUsers = await sut.findAllUsers()
     expect(allUsers).toHaveLength(2)
   })
 })
