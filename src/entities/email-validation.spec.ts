af76646f1e39c7398f552457f6f42202566cec82
Dev Theofanes <devtheofanes@gmail.com>
2023-07-26 08:03:14 -0300

    test: validate e-mail larger than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -25,4 +25,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@email.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept string larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
