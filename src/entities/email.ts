e53a4c32114a307bce02f77f01d7c2d9d9f9416a
Dev Theofanes <devtheofanes@gmail.com>
2023-07-26 07:57:11 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,6 +2,9 @@ export class Email {
   static validate (email?: string | null): boolean {
     if (!email) return false
 
+    const [local] = email.split('@')
+    if (local.length > 64) return false
+
     return true
   }
 }
