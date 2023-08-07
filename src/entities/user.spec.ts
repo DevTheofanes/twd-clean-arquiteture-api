b528b05a08b27830722f50ce8efb1f1e9eedeaf5
Dev Theofanes <devtheofanes@gmail.com>
2023-08-07 06:22:34 -0300

    test: verify e-mail address when creating a user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,8 +1,12 @@
+import { left } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+import { User } from './user'
+
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
-    // const invalidEmail = 'invalid_email'
-    // const userError = User.create({ name: 'any name', email: invalidEmail })
+    const invalidEmail = 'invalid_email'
+    const userError = User.create({ name: 'any name', email: invalidEmail })
 
-    // expect(userError).toBeFalsy()
+    expect(userError).toEqual(left(new InvalidEmailError()))
   })
 })
