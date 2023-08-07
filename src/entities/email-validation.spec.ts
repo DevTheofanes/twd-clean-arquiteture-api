3a45264086e1ffadb690cd5468489bb83acb958c
Dev Theofanes <devtheofanes@gmail.com>
2023-08-07 05:59:48 -0300

    refactor: move user-data model to entities layer

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -46,7 +46,7 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test.only('should not accept domain with a part larger than 63 chars', () => {
+  test('should not accept domain with a part larger than 63 chars', () => {
     const email = 'local@' + 'd'.repeat(64) + '.com'
     expect(Email.validate(email)).toBeFalsy()
   })
---
+++ src/entities/user.spec.ts
@@ -0,0 +1,8 @@
+describe('User domain entity', () => {
+  test('should not create user with invalid e-mail address', () => {
+    // const invalidEmail = 'invalid_email'
+    // const userError = User.create({ name: 'any name', email: invalidEmail })
+
+    // expect(userError).toBeFalsy()
+  })
+})
